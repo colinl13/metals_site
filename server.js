e9842bcc7f2d9b2d88bb8e9c9c3647dcf5e8bdda
colinl13 <clemire@wpi.edu>
2025-12-24 12:35:47 -0500

    made navbar layout with bootstrap styling, added routing for account page

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mime = require("mime");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const { MongoClient } = require("mongodb")
+const { MongoClient } = require("mongodb");
 
 // Middleware to parse JSON bodies
-app.use(express.json())
+app.use(express.json());
 
 // Middleware to serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,21 +16,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-async function start() {
-  try {
-    console.log(`Connecting to MongoDB at ${mongoUri}, db: ${mongoDbName}`)
-    const client = new MongoClient(mongoUri)
-    await client.connect()
-    const db = client.db(mongoDbName)
-    todosCollection = db.collection("todos")
+app.get('/account', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'account.html'))
+})
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`)
-    })
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error)
-    process.exit(1)
-  }
-}
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
-start()
+// async function start() {
+//   try {
+//     console.log(`Connecting to MongoDB at ${mongoUri}, db: ${mongoDbName}`)
+//     const client = new MongoClient(mongoUri)
+//     await client.connect()
+//     const db = client.db(mongoDbName)
+//     todosCollection = db.collection("todos")
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server running on port ${process.env.PORT}`)
+//     })
+//   } catch (error) {
+//     console.error("Failed to connect to MongoDB:", error)
+//     process.exit(1)
+//   }
+// }
+
+// start()
